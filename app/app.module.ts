50a8727021809ed6bb4d75ff934f3869296b30a3
mescalitox <mescalito_mail@yahoo.fr>
2016-11-24 16:27:40 +0100

    tuto rooting

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,41 +1,25 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { RouterModule }   from '@angular/router';
-
-import { AppComponent }  from './app.component';
-import { HeroesComponent }  from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import {HeroService} from './hero.service';
-import {DashboardComponent} from './dashboard.component';
-
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+import { AppRoutingModule }     from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroesComponent,
     DashboardComponent,
-	  HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap: [ AppComponent ],
-  providers: [HeroService]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
